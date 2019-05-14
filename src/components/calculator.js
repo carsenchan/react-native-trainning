6816e8ae5acc1167ae301e37266bc84bb89b6af6
Carsen Chan <kcchane321@gmail.com>
2019-05-14 19:57:45 +0800

    Remove dulplicate structure

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -7,6 +7,114 @@ import {
   SafeAreaView
 } from "react-native";
 
+const calculatorKeys = [
+  [
+    {
+      text: "C",
+      keyType: "symbol",
+      isZero: false
+    },
+    {
+      text: "+/-",
+      keyType: "symbol",
+      isZero: false
+    },
+    {
+      text: "%",
+      keyType: "symbol",
+      isZero: false
+    },
+    {
+      text: "÷",
+      keyType: "operator",
+      isZero: false
+    }
+  ],
+  [
+    {
+      text: "7",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "8",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "9",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "X",
+      keyType: "operator",
+      isZero: false
+    }
+  ],
+  [
+    {
+      text: "4",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "5",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "6",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "-",
+      keyType: "operator",
+      isZero: false
+    }
+  ],
+  [
+    {
+      text: "1",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "2",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "3",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "+",
+      keyType: "operator",
+      isZero: false
+    }
+  ],
+  [
+    {
+      text: "0",
+      keyType: "number",
+      isZero: true
+    },
+    {
+      text: ".",
+      keyType: "number",
+      isZero: false
+    },
+    {
+      text: "=",
+      keyType: "operator",
+      isZero: false
+    }
+  ]
+];
+
 export default class Calculator extends Component {
   state = {
     normalRadius: 0
@@ -30,190 +138,9 @@ export default class Calculator extends Component {
           <Text style={{ color: "white", fontSize: 30 }}>45</Text>
         </View>
         <View style={{ flex: 4, marginTop: 10 }}>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              backgroundColor: "black",
-              justifyContent: "space-between",
-              width: "100%"
-            }}
-          >
-            <Button
-              text={"C"}
-              normalStyle={stylesSymbol.container}
-              onPressStyle={stylesSymbol.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"C"}
-              normalStyle={stylesSymbol.container}
-              onPressStyle={stylesSymbol.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"%"}
-              normalStyle={stylesSymbol.container}
-              onPressStyle={stylesSymbol.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"%"}
-              normalStyle={stylesOperator.container}
-              onPressStyle={stylesOperator.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-          </View>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              backgroundColor: "black",
-              justifyContent: "space-between",
-              width: "100%"
-            }}
-          >
-            <Button
-              text={"7"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"8"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"9"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"X"}
-              normalStyle={stylesOperator.container}
-              onPressStyle={stylesOperator.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-          </View>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              justifyContent: "space-between",
-              backgroundColor: "black",
-              width: "100%"
-            }}
-          >
-            <Button
-              text={"4"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"5"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"6"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"-"}
-              normalStyle={stylesOperator.container}
-              onPressStyle={stylesOperator.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-          </View>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              backgroundColor: "black",
-              justifyContent: "space-between",
-              width: "100%"
-            }}
-          >
-            <Button
-              text={"1"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"2"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"3"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"+"}
-              normalStyle={stylesOperator.container}
-              onPressStyle={stylesOperator.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-          </View>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              backgroundColor: "black",
-              justifyContent: "space-between",
-              width: "100%"
-            }}
-          >
-            <Button
-              text={"0"}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              isZero={true}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"."}
-              normalStyle={stylesNumber.container}
-              onPressStyle={stylesNumber.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-            <Button
-              text={"="}
-              normalStyle={stylesOperator.container}
-              onPressStyle={stylesOperator.onPress}
-              normalRadius={this.state.normalRadius}
-              updateNormailRadius={this.updateNormailRadius}
-            />
-          </View>
+          {calculatorKeys.map((keys, index) => {
+            return <Row keys={keys} key={`row-${index}`} />;
+          })}
         </View>
       </SafeAreaView>
     );
@@ -227,19 +154,54 @@ const styles = StyleSheet.create({
   }
 });
 
-const Button = ({
-  text,
-  isZero,
-  normalStyle,
-  onPressStyle,
-  normalRadius,
-  updateNormailRadius
-}) => {
+const Row = ({ keys }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "black",
+        justifyContent: "space-between",
+        width: "100%"
+      }}
+    >
+      {keys.map((key, index) => (
+        <Button
+          text={key.text}
+          isZero={key.isZero}
+          keyType={key.keyType}
+          key={`${key.text}-${index}`}
+        />
+      ))}
+    </View>
+  );
+};
+
+const Button = ({ text, isZero, keyType }) => {
   const [isPress, setIsPress] = useState(false);
   const [layoutWidth, setLayoutWidth] = useState(0);
   const layoutEnvet = ({ nativeEvent }) => {
     setLayoutWidth(nativeEvent.layout.width);
-    if (!isZero) updateNormailRadius(nativeEvent.layout.width / 2);
+    // if (!isZero) updateNormailRadius(nativeEvent.layout.width / 2);
+  };
+  const buttonStyle = () => {
+    switch (keyType) {
+      case "symbol":
+        return {
+          normalStyle: stylesSymbol.container,
+          onPressStyle: stylesSymbol.onPress
+        };
+      case "operator":
+        return {
+          normalStyle: stylesOperator.container,
+          onPressStyle: stylesOperator.onPress
+        };
+      case "number":
+        return {
+          normalStyle: stylesNumber.container,
+          onPressStyle: stylesNumber.onPress
+        };
+    }
   };
   return (
     <TouchableWithoutFeedback
@@ -248,23 +210,35 @@ const Button = ({
     >
       <View
         style={[
-          normalStyle,
+          styleButton.container,
+          buttonStyle().normalStyle,
           {
-            height: normalRadius * 2,
             borderRadius: layoutWidth / 2
           },
-          isZero ? { width: "42.5%", height: normalRadius * 2 } : {},
-          isPress ? onPressStyle : null
+          isZero
+            ? {
+                width: "42.5%",
+                height: layoutWidth / 2
+              }
+            : { height: layoutWidth },
+          isPress ? buttonStyle().onPressStyle : null
         ]}
         onLayout={layoutEnvet}
       >
-        <Text>{text}</Text>
+        <Text
+          style={[
+            styleButton.text,
+            { fontSize: isZero ? layoutWidth / 8 : layoutWidth / 4 }
+          ]}
+        >
+          {text}
+        </Text>
       </View>
     </TouchableWithoutFeedback>
   );
 };
 
-const stylesNumber = StyleSheet.create({
+const styleButton = StyleSheet.create({
   container: {
     margin: "1.25%",
     width: "20%",
@@ -272,8 +246,16 @@ const stylesNumber = StyleSheet.create({
     alignItems: "center",
     borderRadius: 15,
     borderWidth: 1,
-    borderColor: "#575756",
+    borderColor: "#575756"
+  },
+  text: {
     color: "white",
+    fontWeight: "bold"
+  }
+});
+
+const stylesNumber = StyleSheet.create({
+  container: {
     backgroundColor: "#575756"
   },
   onPress: {
@@ -283,14 +265,6 @@ const stylesNumber = StyleSheet.create({
 
 const stylesOperator = StyleSheet.create({
   container: {
-    margin: "1.25%",
-    width: "20%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 15,
-    borderWidth: 1,
-    borderColor: "black",
-    color: "white",
     backgroundColor: "#f39200"
   },
   onPress: {
@@ -300,14 +274,6 @@ const stylesOperator = StyleSheet.create({
 
 const stylesSymbol = StyleSheet.create({
   container: {
-    margin: "1.25%",
-    width: "20%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 15,
-    borderWidth: 1,
-    borderColor: "black",
-    color: "white",
     backgroundColor: "#808080"
   },
   onPress: {
